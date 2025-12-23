c2aa1e32d2e014c2961221fc4dad0b1990c2c3a0
ghosharundhati89-ui <ghosharundhati89@gmail.com>
2025-12-23 18:06:11 +0600

    Add files via upload

--- mypy.py
+++ mypy.py
@@ -9,4 +9,11 @@ f1.write("codingal is in a mission to teach coding")
 f1.close()
 f1=open("codingal.txt","r")
 print(f1.read())
+c=0
+content=f1.read()
+olist=content.split("\n")
+for lines in olist:
+    if lines:
+      c+=1
+print(c)
 f1.close()
